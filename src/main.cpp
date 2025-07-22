3a3e8792e703df809f68f2a334896d82e71a0503
Michiel <michiel.rhoen@gmail.com>
2025-07-22 21:54:41 +0200

    Direction/position problem resolved

--- src/main.cpp
+++ src/main.cpp
@@ -1,46 +1,138 @@
-//  Let op richting, teller en verschil met eerdere versie.
-
-
 //  Libraries
 #include <Arduino.h>
 #include <LCD_I2C.h>
 #include <I2CKeyPad.h>
 
-//  Proprietary types
+//  Custom types
 enum ElevatorStates {STOPPED, ACCELERATING, DECELERATING, MOVING};
 
 //  Global Const
 const int A1A = 11;
 const int A1B = 12;
 const int posSensorPin = 2;
+const byte upward = false; // upward was forward
+const byte downward = true; // downward was backward
 
-//  Initialize I2C LCD and Keypad objects
+//  I2C LCD and Keypad objects
 LCD_I2C lcd(0x27, 16, 2); // I2C address, columns, rows
 I2CKeyPad keyPad(0x20); // I2C address
 
-
+//  Global vars
 int speed = 0;           
 int position = 100;        
-bool direction = false;   
-//  int destination = 0;     
-
-//  interface, output
-bool lcdFlag = false;     //  for tracking if LCD needs updating
-int count = 0;            //  test variable for showing increasing value on display
-
+bool direction = upward;   
+int destination = 0;     
+bool lcdFlag = false;
+int count = 0;
 ElevatorStates elevatorState = STOPPED;
 
-//  Functions
+//  Function declarations
+void motorA(int s, bool d);
+void changePosition();
+void toggleDirection();
+void lcdPrint();
+void doLCD();
 
+void setup() {
+  pinMode(posSensorPin, INPUT_PULLUP);
+  pinMode(A1A, OUTPUT);
+  pinMode(A1B, OUTPUT);
+
+  attachInterrupt(digitalPinToInterrupt(posSensorPin),
+                  changePosition,
+                  CHANGE);
+
+
+  Serial.begin(9600);
+  Serial.println("go!");
+
+  lcd.begin();
+  lcd.backlight();  //backlight ON.. off with /noBacklight
+  lcd.setCursor(0, 0);
+  lcd.print(F("Go!"));
+  lcd.setCursor(0, 1);
+
+  Wire.begin();
+  Wire.setClock(400000);
+ 
+  if (keyPad.begin() == true) {lcd.print(F("KEYPAD SUCCESS"));}
+  else  {lcd.print(F("KEYPAD FAIL"));}
+}
+
+void loop() {
+
+  // op gang komen
+  Serial.print(position);
+  Serial.print (' ');
+  delay(2500);
+  motorA(155, direction);
+  delay(2500);
+
+  // Volle snelheid
+
+  motorA(255, direction);
+  delay(2500);
+
+  // afremmen
+  motorA(155, direction);
+  delay(2500);
+  
+  // stoppen
+  motorA(0, direction);
+  doLCD();
+
+  // Omkeren
+  toggleDirection();
+  
+  // De andere kant op
+   Serial.println(position);
+  delay(2500);
+  motorA(155, direction);
+
+  // Volle snelheid
+
+  delay(2500);
+  motorA(255, direction);
+
+  // Afremmen
+
+  delay(2500);
+  motorA(155, direction);
+
+  // Stoppen
+  
+  delay(1000);
+  motorA(0, direction);
+  doLCD();
+  
+  toggleDirection();
+
+
+}
+
+// Function definitions
 void motorA(int s, bool d)  {
-  if (d == true) {
+  if (d == downward) {
     analogWrite (A1A, 255-s);
     digitalWrite(A1B, HIGH);
   }
-  if (d == false) {
+  if (d == upward) {
     analogWrite (A1A, s);
     digitalWrite (A1B, LOW);
   }
+}
+
+void changePosition () {
+  if (direction == downward)  {
+    position--;
+  }
+  else  {
+    position++;
+  }
+}
+
+void toggleDirection()  {
+ direction = !direction;
 }
 
 void lcdPrint() {
@@ -54,103 +146,9 @@ void lcdPrint() {
   lcdFlag = false;
 }
 
-void changePosition () {
-  if (direction == true)  {
-    position--;
-  }
-  else  {
-    position++;
-  }
-}
-
-void toggleDirection()  {
- direction = !direction;
-}
-
 void doLCD()  {
   lcd.clear();
   lcd.print(position);
   lcd.print(F(" "));
   lcd.print(direction);
-
-}
-
-void setup() {
-  // put your setup code here, to run once:
-
-  pinMode(posSensorPin, INPUT_PULLUP);
-
-  attachInterrupt(digitalPinToInterrupt(posSensorPin),
-                  changePosition,
-                  CHANGE);
-  pinMode(A1A, OUTPUT);
-  pinMode(A1B, OUTPUT);
-
-  // Serial.begin(9600);
-  // Serial.println("go!");
-
-  // LCD start message
-  lcd.begin();
-  lcd.backlight();  //backlight ON.. off with /noBacklight
-  lcd.setCursor(0, 0);
-  lcd.print(F("Go!"));
-  delay(1000);
-
-  // Initialize Keypad
-  Wire.begin();
-  Wire.setClock(400000);
-  lcd.setCursor(0, 1);
-
-  if (keyPad.begin() == true)
-  {
-    lcd.print(F("KEYPAD SUCCESS"));
-  }
-  else  {
-    lcd.print(F("KEYPAD FAIL"));
-  }
-
-  direction = false;
-
-
-}
-
-void loop() {
-
-  // Op gang komen
-  motorA(155, direction);
-  delay(2500);
-  doLCD();
-
-  // Volle snelheid
-  motorA(255, direction);
-  delay(2500);
-  doLCD();
-
-  // Afremmen
-  motorA(155, direction);
-  delay(2500);
-  doLCD();
-
-  // Stoppen en Omkeren
-   motorA(0, direction);
-   toggleDirection();
-   delay(1000);
-  
-  // De andere kant op
-  motorA(155, direction);
-  delay(2500);
-  doLCD();
-
-  motorA(255, direction);
-  delay(2500);
-  doLCD();
-
-  motorA(155, direction);
-  delay(1000);
-  doLCD();
-
-  motorA(0, direction);
-  delay(1000);
-
-
 }
