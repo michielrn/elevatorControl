20cab67440bc792922922fd0c291d43e8931093a
Michiel <michiel.rhoen@gmail.com>
2025-08-10 22:46:08 +0200

    UNTESTED keypad handling

--- src/main.cpp
+++ src/main.cpp
@@ -29,113 +29,32 @@ int value = 0;
 bool lcdFlag = false;
 byte count = 0;
 ElevatorStates elevatorState = STOPPED;
-int keyPadValue = 0;
 
-//  Function declarations
-void motorA(int s, int d);
-void changePosition();
-void toggleDirection();
-char handleKeypadInput (int &value);
-void lcdPrint();
-void doLCD();
-void elevatorMachine();
+
+// Function declarations
+// void motorA(int s, int d);
+// void changePosition();
+// void toggleDirection();
+// // char handleKeyPadValue(value);
+// void lcdPrint();
+// void doLCD();
+// void elevatorMachine();
 
 // Main program
-
-void setup() {
-  pinMode(posSensorPin, INPUT_PULLUP);
-  pinMode(A1A, OUTPUT);
-  pinMode(A1B, OUTPUT);
-
-  attachInterrupt(digitalPinToInterrupt(posSensorPin),
-                  changePosition,
-                  CHANGE);
-
-
-  Serial.begin(9600);
-  Serial.println("go!");
-
-  lcd.begin();
-  lcd.backlight();  //backlight ON.. off with /noBacklight
-  lcd.setCursor(0, 0);
-  lcd.print(F("Go!"));
-  lcd.setCursor(0, 1);
-
-  Wire.begin();
-  Wire.setClock(400000);
- 
-  if (keyPad.begin() == true) {lcd.print(F("KEYPAD SUCCESS"));}
-  else  {lcd.print(F("KEYPAD FAIL"));}
-
-  elevatorState = GET_INPUT;
-}
-
-void loop() {
-
-  elevatorMachine();
-
-  // // op gang komen
-  // Serial.print(position);
-  // Serial.print (' ');
-  // delay(2500);
-  // motorA(155, direction);
-  // delay(2500);
-
-  // // Volle snelheid
-
-  // motorA(255, direction);
-  // delay(2500);
-
-  // // afremmen
-  // motorA(155, direction);
-  // delay(2500);
-  
-  // // stoppen
-  // motorA(0, direction);
-  // doLCD();
-
-  // // Omkeren
-  // toggleDirection();
-  
-  // // De andere kant op
-  //  Serial.println(position);
-  // delay(2500);
-  // motorA(155, direction);
-
-  // // Volle snelheid
-
-  // delay(2500);
-  // motorA(255, direction);
-
-  // // Afremmen
-
-  // delay(2500);
-  // motorA(155, direction);
-
-  // // Stoppen
-  
-  // delay(1000);
-  // motorA(0, direction);
-  // doLCD();
-  
-  // toggleDirection();
-
-  // while(1);
-
-
-}
 
 // Function definitions
 void elevatorMachine(){
   switch (elevatorState)  {
     case STOPPED: {
       delay(2000);
+      lcd.clear();
       elevatorState = GET_INPUT;
       break;
     }
     case GET_INPUT: {
-      char c = handleKeypadInput(value);
-      lcd.print(c);
+      while (destination == position) {break;}
+      elevatorState = ACCELERATING;
+      break;
     }
     case ACCELERATING:  {
       break;
@@ -192,7 +111,8 @@ void doLCD()  {
 
 }
 
-char handleKeypadInput(int &value) {
+char handleKeyPadValue(int &value)
+{
   char v[19] = "123A456B789C*0#DNF";  //  N = NoKey, F = Fail
   static uint8_t lastKey = 0;
 
@@ -207,7 +127,6 @@ char handleKeypadInput(int &value) {
       case '0' ... '9':
         value *= 10;
         value += c - '0';
-        return c;
         break;
       case '*':
         if (value > 0) value /= 10;
@@ -215,8 +134,19 @@ char handleKeypadInput(int &value) {
       case '#':
         value = 0;
         break;
-      case 'A' ... 'D':
-        //  e.g. store value in EEPROM
+      case 'A':
+        destination = position + value;
+        lcdFlag = true;
+        break;
+      case 'B':
+        destination = position - value;
+        lcdFlag = true;
+        break;
+      case 'C':
+        break;
+      case 'D':
+        destination = value;
+        lcdFlag = true;
         break;
       case 'F':
         Serial.println("FAIL");
@@ -228,5 +158,92 @@ char handleKeypadInput(int &value) {
         break;
     }
   }
+  return c;
+}
+
+
+void setup() {
+  pinMode(posSensorPin, INPUT_PULLUP);
+  pinMode(A1A, OUTPUT);
+  pinMode(A1B, OUTPUT);
+
+  attachInterrupt(digitalPinToInterrupt(posSensorPin),
+                  changePosition,
+                  CHANGE);
+
+
+  Serial.begin(9600);
+  Serial.println("go!");
+
+  lcd.begin();
+  lcd.backlight();  //backlight ON.. off with /noBacklight
+  lcd.setCursor(0, 0);
+  lcd.print(F("Go!"));
+  lcd.setCursor(0, 1);
+
+  Wire.begin();
+  Wire.setClock(400000);
+ 
+  if (keyPad.begin() == true) {lcd.print(F("KEYPAD SUCCESS"));}
+  else  {lcd.print(F("KEYPAD FAIL"));}
+
+  elevatorState = GET_INPUT;
+}
+
+void loop() {
+
+  elevatorMachine();
+  char c = handleKeyPadValue(value);
+  lcdPrint();
+  Serial.println(c);
+
+  // // op gang komen
+  // Serial.print(position);
+  // Serial.print (' ');
+  // delay(2500);
+  // motorA(155, direction);
+  // delay(2500);
+
+  // // Volle snelheid
+
+  // motorA(255, direction);
+  // delay(2500);
+
+  // // afremmen
+  // motorA(155, direction);
+  // delay(2500);
+  
+  // // stoppen
+  // motorA(0, direction);
+  // doLCD();
+
+  // // Omkeren
+  // toggleDirection();
+  
+  // // De andere kant op
+  //  Serial.println(position);
+  // delay(2500);
+  // motorA(155, direction);
+
+  // // Volle snelheid
+
+  // delay(2500);
+  // motorA(255, direction);
+
+  // // Afremmen
+
+  // delay(2500);
+  // motorA(155, direction);
+
+  // // Stoppen
+  
+  // delay(1000);
+  // motorA(0, direction);
+  // doLCD();
+  
+  // toggleDirection();
+
+  // while(1);
+
 
 }
